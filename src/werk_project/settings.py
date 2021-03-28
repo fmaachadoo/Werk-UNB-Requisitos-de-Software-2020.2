a211ee357d06a6e42ed197d3cba0220ec4939485
AlGouvea <alvarohsgouvea@gmail.com>
2021-03-28 06:32:20 -0300

    created templates an initial routes

--- src/werk_project/settings.py
+++ src/werk_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'werk.apps.WerkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
