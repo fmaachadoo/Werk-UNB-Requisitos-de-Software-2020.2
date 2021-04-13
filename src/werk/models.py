205c6dc65a9dd345202926f6d570f60f75f9d1ff
Filipe Machado <filipe__machado@hotmail.com>
2021-04-13 08:53:58 -0300

    Add models docstring
    Co-authored-by: Ailton Aires <ailtonaires5@gmail.com>

--- src/werk/models.py
+++ src/werk/models.py
@@ -3,14 +3,23 @@ from django.contrib.auth.models import User
 
 
 class Workspace(models.Model):
+    """
+        User's Workspace models
+    """
     title = models.CharField(default='Workspace')
 
 
 class Activity(models.Model):
+    """
+        User's activity model
+    """
     workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
 
 
 class WerkUser(User):
+    """
+        Werk's User Model
+    """
     workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
     archived_workspace = models.OneToOneField(Workspace, on_delete=models.CASCADE, primary_key=True)
