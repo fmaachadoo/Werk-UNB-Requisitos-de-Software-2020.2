978e9ad919389e29e3bebba4e2b14a3de384c809
Filipe Machado <filipe__machado@hotmail.com>
2021-04-13 15:11:36 -0300

    Add functional user auth
    Co-authored-by: Ailton Aires <ailtonaires5@gmail.com>

--- src/werk/views.py
+++ src/werk/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
-# Create your views here.
-def home(request):
+
+def homeView(request):
+
     return render(request, 'home.html')
 
-def login(request):
+
+def loginView(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # This method of user authentication is deprecated and should not be considered a secure method
+        # This project is only for purposes of software engineering learning
+        # So, there is no need for it to be a secure project
+        user = User.objects.filter(username=username, password=password).first()
+        if user is not None:
+            login(request, user)
+            return render(request, 'home.html')
     return render(request, 'login.html')
 
-def cadastro(request):
+
+def cadastroView(request):
     return render(request, 'cadastro.html')
