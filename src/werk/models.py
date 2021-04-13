8c41a4d086ef1db641dd925966a75e4ce1214109
Filipe Machado <filipe__machado@hotmail.com>
2021-04-13 08:44:39 -0300

    Co-authored-by: Ailton Aires <ailtonaires5@gmail.com>

--- src/werk/models.py
+++ src/werk/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Workspace(models.Model):
+    title = models.CharField(default='Workspace')
+
+
+class Activity(models.Model):
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
+
+
+class WerkUser(User):
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+    archived_workspace = models.OneToOneField(Workspace, on_delete=models.CASCADE, primary_key=True)
